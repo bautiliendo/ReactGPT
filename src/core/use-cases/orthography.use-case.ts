332fe3ff559d15c2f1217bf9be439cac0f9ccd37
bautiliendo <juanbautistaliendo1@gmail.com>
2024-05-16 15:42:19 -0300

    Avances front orthography

--- src/core/use-cases/orthography.use-case.ts
+++ src/core/use-cases/orthography.use-case.ts
@@ -1,12 +1,26 @@
+import { OrthographyResponse } from "../../interfaces";
+
  
  
  export const orthographyUseCase = async( prompt: string ) => {
     try {
 
-        const resp = await fetch() 
+        const resp = await fetch(`${ import.meta.env.VITE_GPT_API }/orthography-check`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ prompt })
+        });
 
+        if ( !resp.ok ) throw new Error('No se pudo realizar la correción');
 
+        const data = await resp.json() as OrthographyResponse;
 
+        return {
+            ok: true,
+            ...data,
+        }
 
     } catch (error) {
         return {
--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
-import { GptMessage,MyMessage,TextMessageBox, TypingLoader } from "../../components";
+import { GptMessage,GptOrthographyMessage,MyMessage,TextMessageBox, TypingLoader } from "../../components";
+import { orthographyUseCase } from "../../../core/use-cases";
 
 
 interface Message {
   text: string;
   isGpt: boolean;
+  info?: {
+    userScore: number;
+    errors: string[];
+    message: string;
+  }
 }
 
 export const OrthographyPage = () => {
@@ -16,12 +22,26 @@ export const OrthographyPage = () => {
 
     setIsLoading(true);
     setMessages ((prev) => [...prev, {text: text, isGpt: false}] );
-    //TODO: UseCase
 
-    setIsLoading(false);
+    
+    const { ok, errors, message, userScore } = await orthographyUseCase(text);
+    if ( !ok ) {
+      setMessages( (prev) => [...prev, {text: 'No se pudo realizar la correción', isGpt: true}] );
+    } else {
+      setMessages( (prev) => [...prev, {
+        text: message, isGpt: true,
+        info: {
+          errors,
+          message,
+          userScore,
+        }
+      }]);
+    }
+
+    
 
     //Todo Añadir el mensaje de isgppt en true
-
+    setIsLoading(false);
   }
 
   return (
@@ -34,7 +54,10 @@ export const OrthographyPage = () => {
 
           {messages.map((message, index) =>
             message.isGpt ? (
-              <GptMessage key={index} text="Esto es de OpenIA " />
+              <GptOrthographyMessage 
+              key={ index } 
+              {...message.info!}
+              />
             ) : (
               <MyMessage key={index} text={message.text} />
             )
