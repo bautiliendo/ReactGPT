f50836e00416fa0786bdc42b36eb6050ef0ce9ff
bautiliendo <juanbautistaliendo1@gmail.com>
2024-05-16 15:04:33 -0300

    First commit

---
+++ src/core/use-cases/orthography.use-case.ts
@@ -0,0 +1,19 @@
+ 
+ 
+ export const orthographyUseCase = async( prompt: string ) => {
+    try {
+
+        const resp = await fetch() 
+
+
+
+
+    } catch (error) {
+        return {
+            ok: false,
+            userScore: 0,
+            errors: [],
+            message: 'No se pudo realizar la correción'
+        }
+    }
+ }
---
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { GptMessage,MyMessage,TextMessageBox, TypingLoader } from "../../components";
+
+
+interface Message {
+  text: string;
+  isGpt: boolean;
+}
+
+export const OrthographyPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+
+  const handlePost = async( text: string) => {
+
+    setIsLoading(true);
+    setMessages ((prev) => [...prev, {text: text, isGpt: false}] );
+    //TODO: UseCase
+
+    setIsLoading(false);
+
+    //Todo Añadir el mensaje de isgppt en true
+
+  }
+
+  return (
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+          {/* Bienvenida */}
+
+          <GptMessage text="Hola, puedes escribir tu texto en español, y te ayudo con las correciones" />
+
+          {messages.map((message, index) =>
+            message.isGpt ? (
+              <GptMessage key={index} text="Esto es de OpenIA " />
+            ) : (
+              <MyMessage key={index} text={message.text} />
+            )
+          )}
+
+
+          {
+            isLoading && (
+              <div className="col-start-1 col-end-12 fade-in">
+                <TypingLoader />
+              </div>
+            )
+          }
+
+        </div>
+      </div>
+
+      <TextMessageBox
+        onSendMessage={handlePost}
+        placeholder="Escribe aqui lo que deseas"
+        disableCorrections
+      />
+
+
+    </div>
+  );
+};
